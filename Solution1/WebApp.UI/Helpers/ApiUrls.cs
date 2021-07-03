f6076be17177091390eaae3359f6efa419a7ab8a
deepakkhemchandani27 <deepakkhemchandani27@gmail.com>
2021-07-03 16:29:38 +0530

    added meta tags

--- Solution1/WebApp.UI/Helpers/ApiUrls.cs
+++ Solution1/WebApp.UI/Helpers/ApiUrls.cs
@@ -2,8 +2,8 @@
 {
     public static class ApiUrls
     {
-        public const string Rootlocal = "https://localhost:44347";
-      //  public const string Rootlocal = "http://api.robustpackagingeshop.com";
+       // public const string Rootlocal = "https://localhost:44347";
+        public const string Rootlocal = "http://api.robustpackagingeshop.com";
         public static class Identity
         {
             public const string Login = Rootlocal + "/api/user/login";
